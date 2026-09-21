655b11eb82b8aba523af425e4a9342d0736d5b45
Lakshya6373 <dubeylakshya1@gmail.com>
2026-09-21 09:28:14 +0530

    fix: update github oidc thumbprints and handle repo casing

--- terraform/modules/github-oidc/main.tf
+++ terraform/modules/github-oidc/main.tf
@@ -13,7 +13,10 @@ data "tls_certificate" "github" {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f8d7383c4f"
+  ]
 
   tags = {
     Name      = "github-actions-oidc"
@@ -40,7 +43,10 @@ data "aws_iam_policy_document" "github_assume_role" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:${var.github_repo}:*"]
+      values   = [
+        "repo:${var.github_repo}:*",
+        "repo:${lower(var.github_repo)}:*"
+      ]
     }
 
     condition {
